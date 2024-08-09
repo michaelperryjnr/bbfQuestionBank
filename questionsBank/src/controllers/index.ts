6a0bf7c89fa278ad5162c43b82f74da1785ff5c3
Michael Perry Tettey <michaelperryt97@gmail.com>
2024-08-09 18:36:43 +0000

    Add new user controllers

--- questionsBank/src/controllers/index.ts
+++ questionsBank/src/controllers/index.ts
@@ -1,11 +1,21 @@
 import { addQuestion } from "./questionController";
-import { updateUserScores, addUser, getUserScores } from "./userControllers";
+import {
+  updateUserScores,
+  addUser,
+  getUserScores,
+  generateAccessKey,
+  getUsers,
+  verifyAccessKey
+} from "./userControllers";
 
 const User = {
   addQuestion,
   updateUserScores,
   addUser,
   getUserScores,
+  generateAccessKey,
+  getUsers,
+  verifyAccessKey
 };
 
 export { User };
