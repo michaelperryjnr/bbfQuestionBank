fa1ddf372741e5700cde97c77de3036f65c31cd8
Michael Perry Tettey <michaelperryt97@gmail.com>
2024-08-09 18:37:38 +0000

    Update get route

--- questionsBank/src/app/api/users/route.ts
+++ questionsBank/src/app/api/users/route.ts
@@ -2,79 +2,23 @@ import { NextResponse } from "next/server";
 import { User } from "@/controllers";
 import { connectToDb } from "@/db";
 
-export async function GET(request: Request) {
-  await connectToDb();
-  try {
-    const body = await request.json();
-    const userScores = await User.getUserScores(body.username);
-    return NextResponse.json(
-      { userScores },
-      {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        },
-      }
-    );
-  } catch (error: any) {
-    return NextResponse.json(
-      { error: "Error getting user scores" },
-      {
-        status: 500,
-        headers: { "Access-Control-Allow-Origin": "*" },
-      }
-    );
-  }
-}
-
 export async function POST(request: Request) {
   await connectToDb();
   try {
     const body = await request.json();
-    const newUser = await User.addUser(body);
-    return NextResponse.json(newUser, {
-      status: 201,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      },
-    });
-  } catch (error: any) {
-    return NextResponse.json(
-      { error: "Error adding user" },
-      {
-        status: 500,
-        headers: { "Access-Control-Allow-Origin": "*" },
-      }
-    );
+    const user = await User.addUser(body);
+    return NextResponse.json({ sucees: true, message: user.message });
+  } catch (err: any) {
+    return NextResponse.json({ error: "User not added" });
   }
 }
 
-export async function PUT(request: Request) {
+export async function GET(request: Request) {
   await connectToDb();
   try {
-    const body = await request.json();
-    const updatedUserScores = await User.updateUserScores(
-      body.username,
-      body.score
-    );
-    return NextResponse.json(updatedUserScores, {
-      status: 204,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      },
-    });
-  } catch (error: any) {
-    return NextResponse.json(
-      { error: "Error updating user scores" },
-      {
-        status: 500,
-        headers: { "Access-Control-Allow-Origin": "*" },
-      }
-    );
+    const user = await User.getUsers();
+    return NextResponse.json({ sucees: true, message: user });
+  } catch (err: any) {
+    return NextResponse.json({ error: "User not added" });
   }
 }
