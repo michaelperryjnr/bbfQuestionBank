8c52ca4eb6c0bfab78af4d07df0b5c2b0ce8cc93
Michael Perry Tettey <michaelperryt97@gmail.com>
2024-08-09 18:37:20 +0000

    Created validation routes

---
+++ questionsBank/src/app/api/validate/[username]/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse, NextRequest } from "next/server";
+import { BBFUser } from "@/models";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { username: string } }
+) {
+  try {
+    const { username } = params;
+
+    const user = await BBFUser.findOne({ username });
+
+    if (!user) {
+      return NextResponse.json({ isValid: false }, { status: 404 });
+    }
+
+    return NextResponse.json({ isValid: true }, { status: 200 });
+  } catch (error) {
+    console.error("Error validating user:", error);
+    return NextResponse.json(
+      { success: false, message: "Failed to validate user" },
+      { status: 500 }
+    );
+  }
+}
